7fa9e453e4eaa8f7393c0b50ca12cc39291c1a13
Graham Reeves <graham@grahamreeves.com>
2016-05-04 20:55:58 +0100

    First commit. Draws onto a plane with a shader.

---
+++ Assets/PaintMe.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Collider))]
+public class PaintMe : MonoBehaviour {
+
+	public Color ClearColour;
+	public Material PaintShader;
+	public RenderTexture PaintTarget;
+	private RenderTexture TempRenderTarget;
+	private Material ThisMaterial;
+
+	void Init()
+	{
+		if (ThisMaterial == null)
+			ThisMaterial = this.GetComponent<Renderer>().material;
+
+		//	already setup
+		if (PaintTarget != null )
+			if (ThisMaterial.mainTexture == PaintTarget)
+				return;
+
+		//	copy texture
+		if (ThisMaterial.mainTexture != null)
+		{
+			if (PaintTarget == null)
+				PaintTarget = new RenderTexture(ThisMaterial.mainTexture.width, ThisMaterial.mainTexture.height, 0);
+			Graphics.Blit(ThisMaterial.mainTexture, PaintTarget);
+			ThisMaterial.mainTexture = PaintTarget;
+		}
+		else
+		{
+			if (PaintTarget == null)
+				PaintTarget = new RenderTexture(1024,1024, 0);
+
+			//	clear if no existing texture
+			Texture2D ClearTexture = new Texture2D(1, 1);
+			ClearTexture.SetPixel(0, 0, ClearColour);
+			Graphics.Blit(ClearTexture, PaintTarget);
+			ThisMaterial.mainTexture = PaintTarget;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit hitInfo = new RaycastHit();
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		// OnMouseDown
+		if (Input.GetMouseButton(0))
+		{
+			if (Physics.Raycast(ray, out hitInfo))
+			{
+				hitInfo.collider.SendMessage("HandleClick", hitInfo, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+
+	}
+
+	void HandleClick(RaycastHit Hit)
+	{
+		Vector2 LocalHit2 = Hit.textureCoord;
+		PaintAt(LocalHit2);
+	}
+
+
+	void PaintAt(Vector2 Uv)
+	{
+		Init();
+
+		if ( TempRenderTarget == null )
+		{
+			TempRenderTarget = new RenderTexture(PaintTarget.width, PaintTarget.height, 0);
+		}
+		PaintShader.SetVector("PaintUv", Uv);
+		Graphics.Blit(PaintTarget, TempRenderTarget);
+		Graphics.Blit(TempRenderTarget,PaintTarget, PaintShader);
+	}
+}
---
+++ Assets/PaintTexture.shader
@@ -0,0 +1,61 @@
+﻿Shader "NewChromantics/PaintTexture"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		PaintUv("PaintUv", VECTOR) = (0,0,0,0)
+		PaintBrushSize("PaintBrushSize", Range(0,0.1) ) = 0.1
+		PaintBrushColour("PaintBrushColour", Color ) = (1,0,0,1)
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+		
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float PaintBrushSize;
+			float2 PaintUv;
+			float4 PaintBrushColour;
+	
+			
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				if ( distance( i.uv, PaintUv ) < PaintBrushSize )
+					return float4( PaintBrushColour );
+				return tex2D( _MainTex, i.uv );
+			}
+			ENDCG
+		}
+	}
+}
